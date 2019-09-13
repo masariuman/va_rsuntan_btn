1c0a354a32e1974f676b82245345522cc2a137d1
masariuman <masariuman@gmail.com>
2019-09-13 13:20:49 +0700

    selesia pisah

---
+++ resources/views/Crystalline/main.blade.php
@@ -0,0 +1,8 @@
+<div class="app-main">
+    @include('Crystalline.Sidebar')
+    <div class="app-main__outer">
+        @include('Crystalline.Main_Outer')
+        @include('Crystalline.Footer')
+    </div>
+    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+</div>
