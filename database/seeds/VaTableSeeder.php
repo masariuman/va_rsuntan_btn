2cb351d5ab7f4f5b62480a5cd2a41e70999611c9
masariuman <masariuman@gmail.com>
2019-09-19 07:43:20 +0700

    model generate

--- database/seeds/VaTableSeeder.php
+++ database/seeds/VaTableSeeder.php
@@ -12,5 +12,10 @@ class VaTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
