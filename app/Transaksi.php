f434fbc5c815a79b879cd85bc21ef6892d30488d
masariuman <masariuman@gmail.com>
2019-09-19 07:58:17 +0700

    va model

--- app/Transaksi.php
+++ app/Transaksi.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     //
+    protected $table = 'transaksi';
+    protected $fillable = [
+        'user_id',
+        'va_id',
+    	'ref',
+    	'va',
+    	'nama',
+    	'layanan',
+        'kodelayanan',
+        'jenisbayar',
+        'kodejenisbyr',
+        'noid',
+        'tagihan',
+        'flag',
+        'expired',
+        'reserve',
+        'description',
+        'status_inquiry',
+    ];
+    public function user() {
+		return $this->belongsTo('App\User', 'user_id');
+	}
+	public function transaksi() {
+		return $this->hasMany('App\Transaksi', 'va_id');
+	}
 }
