6c50111759e7a15e97da3c2256469a91ce2e20f3
masariuman <masariuman@gmail.com>
2019-10-03 08:48:15 +0700

    DLETE

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -190,4 +190,40 @@ class HomeController extends Controller
         dd($response_decode);
         // dd(json_encode($body));
     }
+
+    public function test_delete()
+    {
+
+        // return view('home');
+        $va = "9457800010100110011";
+        $kode_institusi = 4578;
+        $length_vs = 19;
+        $kode_product = 000;
+        $id = "UNTANWS";
+        $key = "plqQlf6fSoKKBWx4Lxmb0OOMwRKQ3TcN";
+        $secret = "C4UMXATbTT";
+        $signature = "0bf145958638ea90cb1a0162b11b294e02247062f61521818ca166fdc09896ca";
+        $ref = "123456789012";
+        $url_inq = "https://vabtn-dev.btn.co.id:9021/v1/untan/inqVA";
+        $url_create = "https://vabtn-dev.btn.co.id:9021/v1/untan/createVA";
+        $url_update = "https://vabtn-dev.btn.co.id:9021/v1/untan/updVA";
+        $url_delete = "https://vabtn-dev.btn.co.id:9021/v1/untan/deleteVA";
+        $url_report = "https://vabtn-dev.btn.co.id:9021/v1/untan/report";
+
+
+        $body = ['ref' => $ref, 'va' => $va];
+
+
+        $client = new Client([
+            'verify' => false,'headers' => ['Content-Type' => 'application/json','id' => $id, 'key' => $key, 'signature' => $signature]
+        ]);
+
+        $request = $client->post($url_delete,
+            ['body' => json_encode($body)]
+        );
+
+        $response = $request->getBody()->getContents();
+        $response_decode = json_decode($response);
+        dd($response_decode);
+    }
 }
