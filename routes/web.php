0c54a2b5c4b57293458e69b4adb175bb18a4a72f
masariuman <masariuman@gmail.com>
2019-12-19 09:01:21 +0700

    rapikan home

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'InfovarsuntanController@dataInfovarsuntan');
 
 
 Route::get('/', 'InfovarsuntanController@dataInfovarsuntan')->name('/');
